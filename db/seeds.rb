1d0d79cde69d7be14c065f2ba1b3746f9f82c6fa
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-01-23 12:11:29 +0100

    Add index action & view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying ingredient'
+Ingredient.destroy_all
+
+puts 'Destroying cocktail'
+Cocktail.destroy_all
+
+
+lemon = { name: 'lemon' }
+ice = { name: 'ice' }
+mint = { name: 'mint leaves' }
+
+[lemon, ice, mint].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
+
+pina_colada = { name: 'Pina Colada' }
+mojito = { name: 'Mojito' }
+gin = { name: 'Gin' }
+
+[pina_colada, mojito, gin].each do |attributes|
+  cocktail = Cocktail.create!(attributes)
+  puts "Created #{cocktail.name}"
+end
+
+puts 'Finished'
